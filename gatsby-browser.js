4f63bfb368180d347a1b39e8e98368712dec3630
tessereth <tessereth@yahoo.com.au>
2019-03-31 16:04:22 +1100

    Update service worker on route change

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,4 +4,9 @@
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
 
-// You can delete this file if you're not using it
+// Trigger a service worker update on route change
+exports.onRouteUpdate = () => {
+  navigator.serviceWorker.register('/sw.js').then(reg => {
+    reg.update()
+  })
+}
